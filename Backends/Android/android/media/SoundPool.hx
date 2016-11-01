1c1c5e486c1c30dc88c545d6be7aabc1751953e5
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2016-11-01 22:00:29 +0100

    Update `android.media.SoundPool` externs

--- Backends/Android/android/media/SoundPool.hx
+++ Backends/Android/android/media/SoundPool.hx
@@ -3,16 +3,26 @@ package android.media;
 import android.content.res.AssetFileDescriptor;
 
 extern class SoundPool {
-	public function new(channels : Int, type : Int, unknown : Int) : Void;
-	public function load(file : AssetFileDescriptor, unknown : Int) : Int;
-	public function play(id : Int, unknown0 : Int, unknown1 : Int, unknown2 : Int, unknown3 : Int, unknown4 : Int) : Void;
-	public function stop(id : Int) : Void;
+	public function new(maxStreams: Int, streamType: Int, srcQuality: Int): Void;
+	public function autoPause(): Void;
+	public function autoResume(): Void;
+	public function load(fd: AssetFileDescriptor, priority: Int): Int;
+	public function pause(streamID: Int): Void;
+	public function play(soundID: Int, leftVolume: Float, rightVolume: Float, priority: Int, loop: Int, rate: Float): Void;
+	public function release(): Void;
+	public function resume(streamID: Int): Void;
+	public function setLoop(streamID: Int, loop: Int): Void;
+	public function setPriority(streamID: Int, priority: Int): Void;
+	public function setRate(streamID: Int, rate: Float): Void;
+	public function setVolume(streamID: Int, leftVolume: Float, rightVolume: Float): Void;
+	public function stop(streamID: Int): Void;
+	public function unload(soundID: Int): Bool;
 }
 
 @:native("android.media.SoundPool.Builder")
 extern class SoundPoolBuilder {
-	public function new() : Void;
-	public function setMaxStreams(maxStreams : Int) : SoundPoolBuilder;
-	public function setAudioAttributes(attributes : AudioAttributes) : SoundPoolBuilder;
-	public function build() : SoundPool;
+	public function new(): Void;
+	public function setMaxStreams(maxStreams: Int): SoundPoolBuilder;
+	public function setAudioAttributes(attributes: AudioAttributes): SoundPoolBuilder;
+	public function build(): SoundPool;
 }
